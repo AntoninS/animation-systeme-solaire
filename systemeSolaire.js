535d03aa92255c5eb4ab89619005eff9c4cbeb5e
AntoninS <antonin.spontak@gmail.com>
2017-02-15 09:05:31 +0100

    Reglage du décalage de la caméra + ajout pivot (gestion rotation)

--- systemeSolaire.js
+++ systemeSolaire.js
@@ -5,44 +5,85 @@ var renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 renderer.shadowMap.enabled = true;
 document.body.appendChild( renderer.domElement );
-
+var speed = 0.3;
+var zoom = 2;
 camera.position.z = 5;
+camera.position.y = 2;
+camera.lookAt(new THREE.Vector3(0,0,0)); //We set the camera up on the y axis, and make it look the origin
 
-// Initialisation of your objects / materials / light
+// Initialisation of objects / materials / light
 var solarSystem = new THREE.Object3D();
+var pivotSun = new THREE.Object3D();
+var pivotMercury = new THREE.Object3D();
+var pivotVenus = new THREE.Object3D();
+var pivotEarth = new THREE.Object3D();
+var pivotMars = new THREE.Object3D();
+var pivotJupiter = new THREE.Object3D();
+var pivotSaturn = new THREE.Object3D();
+var pivotUranus = new THREE.Object3D();
+var pivotNeptune = new THREE.Object3D();
+var pivotSun = new THREE.Object3D();
 scene.add(solarSystem);
-
+scene.add(pivotSun);
+scene.add(pivotMercury);
+scene.add(pivotVenus);
+scene.add(pivotEarth);
+scene.add(pivotMars);
+scene.add(pivotJupiter);
+scene.add(pivotSaturn);
+scene.add(pivotUranus);
+scene.add(pivotNeptune);
+solarSystem.scale.set(0.4*zoom, 0.4*zoom, 0.4*zoom);
+solarSystem.add(pivotSun);
+solarSystem.add(pivotMercury);
+solarSystem.add(pivotVenus);
+solarSystem.add(pivotEarth);
+solarSystem.add(pivotMars);
+solarSystem.add(pivotJupiter);
+solarSystem.add(pivotSaturn);
+solarSystem.add(pivotUranus);
+solarSystem.add(pivotNeptune);
+var ring = new THREE.RingGeometry( 3.5, 5, 32 );
 var ball = new THREE.SphereGeometry(1, 32, 32);
 
+// SUN
 var sunMaterial = new THREE.MeshPhongMaterial( { } );
 var sun = new THREE.Mesh(ball, sunMaterial);
 sunMaterial.map = THREE.ImageUtils.loadTexture('images/sunmap.jpg')
-sun.scale.set(0.7, 0.7, 0.7);
+sun.scale.set(1*zoom, 1*zoom, 1*zoom);
+pivotSun.add(sun);
 
+// EARTH
 var earthMaterial = new THREE.MeshPhongMaterial( { } );
 var earth = new THREE.Mesh(ball, earthMaterial);
 earthMaterial.map = THREE.ImageUtils.loadTexture('images/earthmap1k.jpg')
+earth.scale.set(0.09*zoom, 0.09*zoom, 0.09*zoom);
+pivotEarth.add(earth);
+earth.position.x = 2.7*zoom;
+earth.castShadow = true; //default is false
+earth.receiveShadow = true;
 
-earth.scale.set(0.3, 0.3, 0.3);
-earth.position.x = 3;
-
+// MOON
 var moonMaterial = new THREE.MeshPhongMaterial( { } );
 var moon = new THREE.Mesh(ball, moonMaterial);
 moonMaterial.map = THREE.ImageUtils.loadTexture('images/moonmap.jpg')
-moon.scale.set(0.15, 0.15, 0.15);
-moon.position.x = 2;
+moon.scale.set(0.1*zoom, 0.1*zoom, 0.1*zoom);
+moon.position.x = 1.5*zoom;
+moon.castShadow = true; //default is false
+moon.receiveShadow = true;
+earth.add(moon);
 
+// MARS
 var marsMaterial = new THREE.MeshPhongMaterial( { } );
 var mars = new THREE.Mesh(ball, marsMaterial);
 marsMaterial.map = THREE.ImageUtils.loadTexture('images/marsmap2k.jpg')
-mars.scale.set(0.3, 0.3, 0.3);
-mars.position.x = 5;
+mars.scale.set(0.045*zoom, 0.045*zoom, 0.045*zoom);
+mars.position.x = 3.2*zoom;
+mars.castShadow = true; //default is false
+mars.receiveShadow = true;
+pivotMars.add(mars);
 
-solarSystem.add(sun);
-sun.add(earth);
-sun.add(mars);
-earth.add(moon);
-
+// LIGHT
 var light = new THREE.PointLight( 0xffffff, 2, 100 );
 light.position.set( 0, 0, 0 );
 scene.add( light );
@@ -63,19 +104,29 @@ scene.add(ambient);
   var mesh  = new THREE.Mesh(geometry, material);
 */
 
-// Test TP2 sur les matrices
+// Moon's following trace material
 var followMaterial = new THREE.MeshBasicMaterial( { color : 0xffffff } );
 
 // This is executed for each frames
 function render() {
     requestAnimationFrame( render );
-
+/*
     sun.rotation.y += 0.01;
     earth.rotation.y += 0.03;
     moon.rotation.y += 0.01;
     mars.rotation.y += 0.01;
+*/
+    pivotEarth.rotation.y += 0.06 *speed;
 
-    //Trace de passage de la lune
+    pivotMars.rotation.y += 0.05 *speed;
+
+    pivotSun.rotation.y += 0.005 *speed;
+
+    earth.rotation.y += 0.03 * speed;
+    sun.rotation.y += 0.01;
+
+
+    // Moon's following trace
     var moonFollow = new THREE.Mesh(ball, followMaterial);
     moonFollow.scale.set(0.01, 0.01, 0.01 );
 
